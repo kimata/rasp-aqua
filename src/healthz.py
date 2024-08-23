c3a9b0e759c9aa9cd521ef6fd72261f30cebe3aa
KIMATA Tetsuya <kimata@green-rabbit.net>
2024-08-23 15:26:59 +0900

    fix

--- src/healthz.py
+++ src/healthz.py
@@ -10,53 +10,46 @@ Options:
   -d                : デバッグモードで動作します．
 """
 
-import datetime
 import logging
 import pathlib
 import sys
 
+import my_lib.healthz
+from docopt import docopt
 
-def check_liveness(config):
-    liveness_file = pathlib.Path(config["liveness"]["file"]["scheduler"])
-    interval_sec = config["liveness"]["interval_sec"]
 
-    if not liveness_file.exists():
-        logging.warning("Not executed.")
-        return False
-
-    elapsed = datetime.datetime.now() - datetime.datetime.fromtimestamp(liveness_file.stat().st_mtime)
-    # NOTE: 少なくとも1分は様子を見る
-    if elapsed.total_seconds() > max(interval_sec * 2, 60):
-        logging.warning(
-            "Execution interval is too long. ({elapsed:,} sec)".format(elapsed=elapsed.total_seconds)
-        )
-        return False
+def check_liveness(target_list):
+    for target in target_list:
+        if not my_lib.healthz.check_liveness(target["name"], target["liveness_file"], target["interval"]):
+            return False
 
     return True
 
 
 ######################################################################
 if __name__ == "__main__":
-    import local_lib.config
-    import local_lib.logger
-    from docopt import docopt
+    import my_lib.config
+    import my_lib.logger
 
     args = docopt(__doc__)
 
     config_file = args["-c"]
     debug_mode = args["-d"]
 
-    if debug_mode:
-        log_level = logging.DEBUG
-    else:
-        log_level = logging.INFO
+    my_lib.logger.init("hems.rasp-aqua", level=logging.DEBUG if debug_mode else logging.INFO)
 
-    local_lib.logger.init("hems.rasp-aqua", level=log_level)
+    logging.info("Using config config: %s", config_file)
+    config = my_lib.config.load(config_file)
 
-    logging.info("Using config config: {config_file}".format(config_file=config_file))
-    config = local_lib.config.load(config_file)
+    target_list = [
+        {
+            "name": "webapp",
+            "liveness_file": pathlib.Path(config["liveness"]["file"]),
+            "interval": config["liveness"]["interval_sec"],
+        }
+    ]
 
-    if check_liveness(config):
+    if check_liveness(target_list):
         logging.info("OK.")
         sys.exit(0)
     else:
