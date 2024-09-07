17a795bf405e8aff55bb3a6d73977c4019c0cc10
KIMATA Tetsuya <kimata@green-rabbit.net>
2024-09-07 19:00:54 +0900

    style: 整形

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ Usage:
   rasp-aqua.py [-c CONFIG] [-d]
 
 Options:
-  -c CONFIG     : CONFIG を設定ファイルとして読み込んで実行します．[default: config.yaml]
+  -c CONFIG         : CONFIG を設定ファイルとして読み込んで実行します．[default: config.yaml]
   -d                : デバッグモードで動作します．
 """
 
